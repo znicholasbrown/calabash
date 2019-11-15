c5c12baa8b08b63ddd3c2dd4dc029399b23dfb2f
nicholas <znicholasbrown@gmail.com>
2019-11-15 12:18:39 -0500

    Update registry_url

--- Tea_Flow_deploy.py
+++ Tea_Flow_deploy.py
@@ -4,7 +4,7 @@ from prefect.environments.storage import Docker
 storage = Docker(
     base_image="python:3.7",
     python_dependencies=[],
-    registry_url="znicholasbrown/prefect_flows",
+    registry_url="docker.io/znicholasbrown/prefect_flows",
     image_name="tea-flow",
     image_tag="tea-flow",
 )
