3f640f7c7fcfe136c9ec1f5f44d41c7081a7755e
nicholas <znicholasbrown@gmail.com>
2019-11-15 12:32:30 -0500

    Update deploy script

--- Tea_Flow_deploy.py
+++ Tea_Flow_deploy.py
@@ -1,12 +1,10 @@
 from Tea_Flow import List_Teas
-from prefect.environments.storage import Docker
 
-storage = Docker(
+List_Teas.deploy(
+    "Flow Schematics", 
     base_image="python:3.7",
     python_dependencies=[],
-    registry_url="docker.io/znicholasbrown/prefect_flows",
+    registry_url="docker.io/znicholasbrown",
     image_name="tea-flow",
     image_tag="tea-flow",
 )
-
-List_Teas.deploy("Flow Schematics", storage)
